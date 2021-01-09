283d81aa4d8f59fb81f60d84847c99d8324f4c7c
mephi007 <roysumit949@gmail.com>
2021-01-09 18:34:06 +0530

    basic test to add two number seperated by , : passed

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -4,5 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringCalculator {
+    public int calculate(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+        String[] chuncks = text.split(",");
+        int total = 0;
+        for (String item : chuncks) {
+            int num = Integer.parseInt(item);
+            total += num;
+        }
+        return total;
+    }
 
 }
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -1,2 +1,14 @@
-package org.example;public class StringCalculatorTest {
+package org.example;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void emptyStringShouldReturnZero() {
+        StringCalculator sut = new StringCalculator();
+        assertEquals(sut.calculate("1,2"), 3);
+    }
+
 }
