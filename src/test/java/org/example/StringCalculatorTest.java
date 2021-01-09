30f429f420893380d147c2212b0867e8370ed54e
mephi007 <roysumit949@gmail.com>
2021-01-09 18:36:47 +0530

    basic scenarios covered : Passed

--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -6,9 +6,25 @@ import static org.junit.Assert.fail;
 public class StringCalculatorTest {
 
     @Test
-    public void emptyStringShouldReturnZero() {
+    public void testTwoNumber() {
         StringCalculator sut = new StringCalculator();
         assertEquals(sut.calculate("1,2"), 3);
+    }
+
+    public void testEmptyStringShouldReturnZero() {
+        StringCalculator sut = new StringCalculator();
+        assertEquals(sut.calculate(""), 0);
+    }
+    @Test
+    public void testOneNumberShouldReturnItseft() {
+        StringCalculator sut = new StringCalculator();
+        assertEquals(sut.calculate("3"), 3);
+    }
+
+    @Test
+    public void testMoreOneDigits() {
+        StringCalculator sut = new StringCalculator();
+        assertEquals(sut.calculate("22,55"), 77);
     }
 
 }
