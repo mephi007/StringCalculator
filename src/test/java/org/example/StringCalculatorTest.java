1f5305e4d564cfe0f3c825db7f3a30cc90d73ca4
mephi007 <roysumit949@gmail.com>
2021-01-09 18:51:46 +0530

    correcting assertion signature, modified code to support more delimiters

--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -8,30 +8,30 @@ public class StringCalculatorTest {
     @Test
     public void testTwoNumber() {
         StringCalculator sut = new StringCalculator();
-        assertEquals(sut.calculate("1,2"), 3);
+        assertEquals(3, sut.calculate("1,2"));
     }
 
     public void testEmptyStringShouldReturnZero() {
         StringCalculator sut = new StringCalculator();
-        assertEquals(sut.calculate(""), 0);
+        assertEquals(0, sut.calculate(""));
     }
     @Test
     public void testOneNumberShouldReturnItseft() {
         StringCalculator sut = new StringCalculator();
-        assertEquals(sut.calculate("3"), 3);
+        assertEquals(3,sut.calculate("3"));
     }
 
     @Test
     public void testMoreOneDigits() {
         StringCalculator sut = new StringCalculator();
-        assertEquals(sut.calculate("22,55"), 77);
+        assertEquals(77, sut.calculate("22,55"));
     }
 
     //Allow the Add method to handle an unknown amount of numbers
     @Test
     public void testUnknownAmountOfNnumersAsInput() {
         StringCalculator sut = new StringCalculator();
-        assertEquals(sut.calculate("1,2,3,4,5,6,8,9"), 38);
+        assertEquals(38, sut.calculate("1,2,3,4,5,6,8,9"));
     }
 
     //Allow the Add method to handle new lines between numbers (instead of commas).
@@ -39,7 +39,13 @@ public class StringCalculatorTest {
     @Test
     public void testNewLineAsDelimiter() {
         StringCalculator sut = new StringCalculator();
-        assertEquals(sut.calculate("1\n2,3"), 6);
+        assertEquals(6, sut.calculate("1\n2,3"));
+    }
+
+    @Test
+    public void supportForCustomDelimiter() {
+        StringCalculator sut = new StringCalculator();
+        assertEquals(10, sut.calculate(";\n1;2;3;4"));
     }
 
 }
